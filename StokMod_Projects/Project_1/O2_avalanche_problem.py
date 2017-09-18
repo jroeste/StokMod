70c3eef4e5f475b321ce1a329bc62bf879987c06
Øystein Hovind <hovindo@gmail.com>
2017-09-18 10:00:49 +0200

    Added things.

--- StokMod_Projects/Project_1/O2_avalanche_problem.py
+++ StokMod_Projects/Project_1/O2_avalanche_problem.py
@@ -1,2 +1,30 @@
 __author__ = 'julie'
 # -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def compute_marginal_probability(P, x0, n):
+	dim = len(x0);
+
+	x = np.zeros((dim, n));
+	
+	x[:,0] = x0;
+	for i in range(0, n - 1):
+		x[:,i + 1] = np.matmul(P, x[:,i]);
+	return x;
+
+def draw_realizations(n):
+	
+	
+def test_marginal_probability():
+	P = [[0.95, 0.00], [0.05, 1]];
+	x0 = [0.99, 0.01];
+
+	n = 50;
+	x = compute_marginal_probability(P, x0, n);
+	plt.plot(range(0,n), x[1,:]);
+	plt.savefig("teest.png");
+	print(x);
+
+test_marginal_probability();
