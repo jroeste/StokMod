d118ab0c8aee2b7012404f040ac65a61646136e9
Øystein Hovind <hovindo@gmail.com>
2017-09-18 11:36:33 +0200

    Started with 2b.

--- StokMod_Projects/Project_1/O2_avalanche_problem.py
+++ StokMod_Projects/Project_1/O2_avalanche_problem.py
@@ -3,6 +3,7 @@ __author__ = 'julie'
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.random as rnd
 
 def compute_marginal_probability(P, x0, n):
 	dim = len(x0);
@@ -14,17 +15,27 @@ def compute_marginal_probability(P, x0, n):
 		x[:,i + 1] = np.matmul(P, x[:,i]);
 	return x;
 
-def draw_realizations(n):
-	
-	
+def draw_realization(P, x0, n):
+	dim = len(x0);
+	x = np.zeros((dim, n));
+
+	x[:,0] = x0;
+	for i in range(0, n - 1):
+		tmp = rnd.rand();
+		p = [tmp, 1 - tmp];
+		x[:,i + 1] = np.less(p, np.matmul(P, x[:,i])).astype(np.float64);
+	return x;
+
 def test_marginal_probability():
 	P = [[0.95, 0.00], [0.05, 1]];
 	x0 = [0.99, 0.01];
 
 	n = 50;
-	x = compute_marginal_probability(P, x0, n);
-	plt.plot(range(0,n), x[1,:]);
-	plt.savefig("teest.png");
-	print(x);
+	#x = compute_marginal_probability(P, x0, n);
+	#plt.plot(range(0,n), x[1,:]);
+	#plt.savefig("teest.png");
+	#print(x);
+	r = draw_realization(P, x0, n);
+	print(r);
 
 test_marginal_probability();
