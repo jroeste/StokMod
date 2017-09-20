3c15d0755f1cab82d0746805d4abe216a741e5c9
Øystein Hovind <hovindo@gmail.com>
2017-09-20 12:42:11 +0200

    Added plot.

--- StokMod_Projects/Project_1/O2_avalanche_problem.py
+++ StokMod_Projects/Project_1/O2_avalanche_problem.py
@@ -30,6 +30,14 @@ def draw_realization(P, x0, n):
 		r[:,i + 1] = sample(p);
 	return r;
 
+def draw_realizations(P, x0, n, r):
+	dim = len(x0);
+	x = np.zeros((r, n));
+
+	for i in range(1, r):
+		x[i,:] = draw_realization(P, x0, n)[0];
+	return x;
+
 def test_marginal_probability():
 	P = [[0.95, 0.00], [0.05, 1]];
 	x0 = [0.99, 0.01];
@@ -39,9 +47,18 @@ def test_marginal_probability():
 	#plt.plot(range(0,n), x[1,:]);
 	#plt.savefig("teest.png");
 	#print(x);
-	r = draw_realization(P, x0, n);
-	print(r);
-	
+
+def test_realizations():
+	P = [[0.95, 0.00], [0.05, 1]];
+	x0 = [0.99, 0.01];
+
+	n = 50;
+	r = 25;
+
+	x = draw_realizations(P, x0, n, r);
+	plt.matshow(x, cmap="hot");
+	plt.show();
+
 def test_cost():
 	P = [[0.95, 0.00], [0.05, 1]];
 	x0 = [0.99, 0.01];
@@ -58,4 +75,5 @@ def test_cost():
 	print(all_cost);
 
 test_marginal_probability();
+test_realizations();
 test_cost();
