2cab3b9260a6c22eef2577065828928cfe350c65
Yohann Jacob Sandvik <yohann.sandvik@revolve.no>
2017-10-22 01:29:46 +0200

    Add simulations 1a.2 and 1b.1

--- StokMod_Projects/Project_2/O1_insurance_claims.py
+++ StokMod_Projects/Project_2/O1_insurance_claims.py
@@ -16,16 +16,60 @@ import random as rnd
 # 	return prob
 
 # print(poissonProb(3,59,0,1)
-def simulateNrTasks(nrSimulations, probLimit):
+def simulateNrTasks(nrSimulations, probLimit, timePeriod, intensity):
 	n = nrSimulations # Number of simulations
-	lam = 3 # Intensity constant
-	t = 59 # Time interval we are interested inn (59 days)
+	lam = intensity # Intensity constant
+	t = timePeriod # Time interval we are interested inn (59 days)
 	N = np.random.poisson(lam=lam*t, size=n) #Sample the number of insurance claims from the poisson distriution.
 	counter = 0
 	for i in range(0,n):
 			if (N[i] > probLimit):
 				counter += 1
 	percentage = (counter/n)*100
-	print(percentage, '%')
+	return percentage
 
-simulateNrTasks(10000,175)
+# print(simulateNrTasks(10000,175,59,3))
+
+def simulate_N_as_func_of_time_A(lamb):
+	N_t = np.zeros(59)
+	for t in range(0,59):
+		N_t[t] = np.random.poisson(lam=lamb*t, size=1)
+	return N_t
+
+def simulate_N_as_func_of_time_B():
+	N_t = np.zeros(59)
+	for t in range(0,59):
+		lamb = 2 + np.cos((np.pi/182.5)*t)
+		N_t[t] = np.random.poisson(lam=lamb*t, size=1)
+	return N_t
+
+# vec = np.zeros((30, 3)) # 3 rader 30 kolonner
+
+def plot_simulations(task):
+	b = 100
+	t = range(0,59)
+	N = np.zeros((100,59))
+	if (task == 1):
+		for i in range(0,b):
+			N[i,:] = simulate_N_as_func_of_time_A(3)
+			plt.plot(t, N[i,:], color='blue')
+		plt.xlabel("t")
+		plt.ylabel("N(t)")
+		plt.title("Probability")
+		plt.grid()
+		return 1
+	elif (task == 2):
+		for i in range(0,b):
+			N[i,:] = simulate_N_as_func_of_time_B()
+			plt.plot(t, N[i,:])
+		plt.xlabel("t")
+		plt.ylabel("N(t)")
+		plt.title("Probability")
+		plt.grid()
+		return 1
+	else:
+		print('What task are you talking about?')
+		return 0
+
+plot_simulations(2)
+plt.show()
