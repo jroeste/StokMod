a1a536f232e78dc9e637d94efacf08f26acad0bc
Øystein Hovind <oysthovi@ntnu.no>
2017-10-22 16:17:05 +0200

    added simple script.

--- StokMod_Projects/Project_2/insurance.py
+++ StokMod_Projects/Project_2/insurance.py
@@ -1,16 +1,13 @@
 import numpy as np
-import numpy
 
-t=59; #Number of days
-lamda=3;
-mu=-2;
-sigma=1; 
-C = [0]*t;
+n=100000 #Number of years
+lam=3;t=59;mu=-2;sigma=1; 
+C=[0]*n
 
 
-for i in range(0,t):
-    N = numpy.random.poisson(lam=lamda, size=1); #Sample the number of insurance claims from the poisson distriution.
-    Z[i] = sum(numpy.random.lognormal(mean=mu, sigma=sigma, size=N)); #Sample the insurance amount claims from the lognormal distribution.
+for i in range(0,n):
+    N=np.random.poisson(lam=lam*t, size=1) #Sample the number of insurance claims from the poisson distriution.
+    C[i]=sum(np.random.lognormal(mean=mu, sigma=sigma, size=N)) #Sample the insurance amount claims from the lognormal distribution.
 
-print("Expected value: ", numpy.mean(C));
-print("Variance: " ,numpy.var(C));
+print("Expected value: ", np.mean(C))
+print("Variance: " ,np.var(C))
