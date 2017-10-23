f0cce4a62564c184d0f35d2190dbab790939e44e
Yohann Jacob Sandvik <yohann.sandvik@revolve.no>
2017-10-23 02:28:42 +0200

    Add plots for 1a and 1b, and simulations for 1b

--- StokMod_Projects/Project_2/O1_insurance_claims.py
+++ StokMod_Projects/Project_2/O1_insurance_claims.py
@@ -33,29 +33,31 @@ def simulateNrTasks(nrSimulations, probLimit, timePeriod, intensity):
 def simulate_N_as_func_of_time_A(lamb):
 	N_t = np.zeros(59)
 	for t in range(0,59):
-		N_t[t] = np.random.poisson(lam=lamb*t, size=1)
+		N_t[t] = np.random.poisson(lam=lamb, size=1) 
+		N_t[t] += N_t[t-1]
 	return N_t
 
 def simulate_N_as_func_of_time_B():
 	N_t = np.zeros(59)
 	for t in range(0,59):
 		lamb = 2 + np.cos((np.pi/182.5)*t)
-		N_t[t] = np.random.poisson(lam=lamb*t, size=1)
+		N_t[t] = np.random.poisson(lam=lamb, size=1)
+		N_t[t] += N_t[t-1]
 	return N_t
 
 # vec = np.zeros((30, 3)) # 3 rader 30 kolonner
 
-def plot_simulations(task):
-	b = 100
-	t = range(0,59)
-	N = np.zeros((100,59))
+def plot_simulations(task, nrSimulations):
+	b = int(nrSimulations)
+	t = np.arange(0,59) 
+	N = np.zeros((b,59))
 	if (task == 1):
 		for i in range(0,b):
 			N[i,:] = simulate_N_as_func_of_time_A(3)
-			plt.plot(t, N[i,:], color='blue')
+			plt.plot(t, N[i,:])
 		plt.xlabel("t")
 		plt.ylabel("N(t)")
-		plt.title("Probability")
+		plt.title("Realizations of N(t) with constant intensity")
 		plt.grid()
 		return 1
 	elif (task == 2):
@@ -64,12 +66,27 @@ def plot_simulations(task):
 			plt.plot(t, N[i,:])
 		plt.xlabel("t")
 		plt.ylabel("N(t)")
-		plt.title("Probability")
+		plt.title("Realizations of N(t) with time dependent intensity")
 		plt.grid()
 		return 1
 	else:
 		print('What task are you talking about?')
 		return 0
 
-plot_simulations(2)
-plt.show()
+# plot_simulations(1,100)
+# plt.show()
+
+def simulateNrTasks2(nrSimulations, probLimit):
+	n = nrSimulations # Number of simulations
+	counter = 0
+	length = 59
+	for i in range(0,n):
+		N_t = simulate_N_as_func_of_time_B()
+		if (N_t[length-1] > probLimit):
+			counter += 1
+	percentage = (counter/n)*100
+	print(percentage)
+
+
+simulateNrTasks2(10000,175)
+# print(simulate_N_as_fun100_of_time_B())
