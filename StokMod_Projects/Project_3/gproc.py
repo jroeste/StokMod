5bd8d64bda6b3ba604110cdd9a3e03b81b08f04e
Øystein Hovind <hovindo@gmail.com>
2017-11-07 11:51:45 +0100

    F.1 and F.2.

--- StokMod_Projects/Project_3/gproc.py
+++ StokMod_Projects/Project_3/gproc.py
@@ -1,23 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sample(tmin, tmax):
-	#tmin = 1;
-	#tmax = 100;
-	n = tmax - tmin + 1;
-
-	mu = 1;
-	sigma = 1;
-
-	t = np.linspace(tmin, tmax, n);
-	ones = np.ones(n);
-	H = abs(np.outer(t, ones) - np.outer(ones, t));
-	S = sigma**2 * np.exp(-phi*H);
-	L = np.linalg.cholesky(S);
-
-	z = np.random.normal(0, 1, n);
-	return mu + L*z;
-
 def h(ta, tb):
 	#t = np.linspace(tmin, tmax, n);
 	na = len(ta);
@@ -31,20 +14,56 @@ def samplemodel(t, phi):
 	mu = 0;
 	sigma = 1;
 
+	n = len(t);
 	H = h(t, t);
 	S = sigma**2 * np.exp(-phi*H);
 	L = np.linalg.cholesky(S);
 
 	z = np.random.normal(0, 1, n);
-	return mu + L*z;
+	return mu + np.matmul(L, z);
+def expocor(H, sigma, phi):
+	return sigma**2 * np.exp(-phi*H);
 	
+def condexpvar(ta, xb, tb, sigma, phi):
+	Ha = h(ta, ta);
+	Hb = h(tb, tb);
+	Hab = h(ta, tb);	
+	Sa = expocor(Ha, sigma, phi);
+	Sb = expocor(Hb, sigma, phi);
+	Sab = expocor(Hab, sigma, phi);
+	tmp = np.matmul(Sab, np.linalg.inv(Sb));
+	condexp = np.matmul(tmp, xb);
+	condvar = Sa - np.matmul(tmp, np.transpose(Sab));
+	return condexp, condvar;
 
 def main():
 	phi = 3/10;
 	tmin = 1;
 	tmax = 100;
+	n = tmax - tmin + 1;
 	t = np.linspace(tmin, tmax, tmax - tmin + 1);
-	for i in range(10):
-		z = sample(t, phi);
-		plt.hist(z);
-		
+
+	N = 10;
+	z = np.zeros((N, n));
+	for i in range(N):
+		z[i,:] = samplemodel(t, phi);
+		plt.plot(t, z[i,:]);
+	plt.show();
+#main();
+
+def testf2():
+	xb = np.array([0.58, -1.34, 0.61]);
+	tb = np.array([11.2, 51.8, 81.4]);
+
+	sigma = 1;
+	phi = 3/10;
+	tmin = 1;
+	tmax = 100;
+	n = tmax - tmin + 1;
+	ta = np.linspace(tmin, tmax, tmax - tmin + 1);
+
+	condexp, condvar = condexpvar(ta, xb, tb, sigma, phi);	
+
+	plt.plot(ta, condexp);
+	plt.show();
+testf2();
